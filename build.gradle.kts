afce67aace5ba91da59ce08e92447890ca283b23
Alex Petitjean <alex@tenforwardconsulting.com>
2021-06-01 22:13:38 -0500

    Set up GradleNexus PublishPlugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.*
+
 buildscript {
     repositories {
         mavenCentral()
@@ -8,11 +10,38 @@ buildscript {
     }
 }
 
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
 allprojects {
     group = project.property("GROUP")!!
     version = project.property("VERSION_NAME")!!
 
     repositories {
         mavenCentral()
+    }
+}
+
+val propertiesFile = file("local.properties")
+val properties = Properties()
+if (propertiesFile.exists()) {
+    properties.load(propertiesFile.inputStream())
+}
+
+val sonatypeUsername = properties["SONATYPE_NEXUS_USERNAME"] as? String
+val sonatypePassword = properties["SONATYPE_NEXUS_PASSWORD"] as? String
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+            if (sonatypeUsername != null && sonatypePassword != null) {
+                username.set(sonatypeUsername)
+                password.set(sonatypePassword)
+            }
+        }
     }
 }
--- unwrapping-converter/build.gradle.kts
+++ unwrapping-converter/build.gradle.kts
@@ -1,9 +1,11 @@
+import org.jetbrains.kotlin.load.kotlin.signatures
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     `java-library`
+    `maven-publish`
+    signing
 }
-
-apply(from = "../gradle/gradle-mvn-push.gradle")
 
 repositories {
     mavenCentral()
@@ -14,7 +16,50 @@ dependencies {
     // TODO: Check these for updates?
     api("com.squareup.retrofit2:retrofit:2.9.0")
     api("com.squareup.retrofit2:converter-moshi:2.9.0")
-    api("com.squareup.okhttp3:okhttp:4.9.0")
-    api("com.squareup.moshi:moshi:1.11.0")
-    // api("com.squareup.okio:okio:2.8.0")
+    api("com.squareup.okhttp3:okhttp:4.9.1")
+    api("com.squareup.moshi:moshi:1.12.0")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set(prop("POM_ARTIFACT_ID"))
+                description.set(prop("POM_DESCRIPTION"))
+                url.set(prop("POM_URL"))
+
+                licenses {
+                    license {
+                        name.set(prop("POM_LICENSE_NAME"))
+                        url.set(prop("POM_LICENSE_URL"))
+                        distribution.set(prop("POM_LICENSE_DIST"))
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set(prop("POM_DEVELOPER_ID"))
+                        name.set(prop("POM_DEVELOPER_NAME"))
+                    }
+                }
+
+                scm {
+                    url.set(prop("POM_SCM_URL"))
+                    connection.set(prop("POM_SCM_CONNECTION"))
+                    developerConnection.set(prop("POM_SCM_DEV_CONNECTION"))
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(*publishing.publications.toTypedArray())
+}
+
+fun prop(name: String): String {
+    return project.property(name) as String
 }
